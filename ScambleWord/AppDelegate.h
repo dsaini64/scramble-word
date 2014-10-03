8ea409362e7a464c331b0fed5196c44ea96222e3
Divakar Saini <dsaini64@gmail.com>
2014-10-02 18:31:25 -0700

    Initial Commit

---
+++ ScambleWord/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScambleWord
+//
+//  Created by Divakar Saini on 10/2/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
